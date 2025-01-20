9a3268cc9884cdf5907624b08df692d75fa8685f
Tushar Dontulwar <150892030+TusharLOL@users.noreply.github.com>
2025-01-20 22:23:01 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,7 +6,7 @@ import { toast } from "sonner";
 import { Link, useNavigate } from "react-router-dom";
 import { Loader2 } from "lucide-react";
 import { useSelector } from "react-redux";
-import logo from "@/assets/instagram_logo.svg";
+import logo from "../assets/instagram_logo.svg";
 
 const Signup = () => {
   const [input, setInput] = useState({
